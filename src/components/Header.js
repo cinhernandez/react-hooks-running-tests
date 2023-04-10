747b664b704cf6eb4e48dba950adb2194928603b
Cindy Hernandez <cindyh@mail.com>
2023-04-09 18:22:44 -0700

    complete lab

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,8 +1,8 @@
 import React from "react";
 
 function Header(props) {
-      return <h1>hello from the Header!</h1>
-      
+      return <h1>hello from the Header!</h1>;
+
 }
 
 export default Header;
